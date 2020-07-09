9ee88463e4216a11bb07a5ddc1ef93816aad667c
I-TatsuyaEP <c1000724@planet.kanazawa-it.ac.jp>
2020-07-09 19:23:45 +0900

    delete Users#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get 'users/show'
   get "/sign_up", to: "users#new"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
